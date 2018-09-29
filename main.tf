9bdf7937e46107997f5976e09901e01c409dc90c
Cherish <shmilycherish@gmail.com>
2018-09-29 11:05:21 +0800

    create instance, security group and deploy static website

--- main.tf
+++ main.tf
@@ -2,12 +2,82 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-
 resource "aws_instance" "server" {
-  ami           = "ami-08569b978cc4dfa10"
-  instance_type = "t2.nano"
+  ami                    = "ami-08569b978cc4dfa10"
+  instance_type          = "t2.nano"
+  vpc_security_group_ids = ["${aws_security_group.cherish.id}"]
+  key_name               = "cherish"
+
+  provisioner "file" {
+    connection {
+      user        = "ec2-user"
+      private_key = "${file("~/Downloads/study/cherish.pem")}"
+    }
+
+    source      = "./html"
+    destination = "/tmp"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      user        = "ec2-user"
+      private_key = "${file("~/Downloads/study/cherish.pem")}"
+    }
+
+    inline = [
+      "sudo yum install -y nginx",
+      "sudo rm -rf /usr/share/nginx/html",
+      "sudo  mv /tmp/html /usr/share/nginx",
+      "sudo service nginx start",
+    ]
+  }
 
   tags {
     Name = "cy-terraform"
   }
+}
+
+resource "aws_security_group" "cherish" {
+  name = "cherish"
+
+  ingress {
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "www" {
+  name    = "cherish.workshop.oc-tw.net"
+  type    = "A"
+  zone_id = "Z2F25J92GG08RZ"
+  ttl     = "900"
+  records = ["${aws_instance.server.public_ip}"]
+}
+
+output "web-public-dns" {
+  value = "${aws_instance.server.public_dns}"
+}
+
+output "web-public-ip" {
+  value = "${aws_instance.server.public_ip}"
 }
